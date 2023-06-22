d46d5f9f6001380e2625a808c36bc76ef46422d8
Viktoriia Boiko <boiko.viktorika@gmail.com>
2023-06-22 17:39:06 +0200

    First structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Challenge</h1>
+			<p>Challenge Migrate this HTML from index.html to the React index.js</p>
+			<small>
+				<a
+					href="https://reactjs.org/"
+					target="_blank"
+					className="important"
+					rel="noreferrer"
+				>
+					Official React Website
+				</a>
+			</small>
+		</div>
+	);
+}
+
+export default App;
